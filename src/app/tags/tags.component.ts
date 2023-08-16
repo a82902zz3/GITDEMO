a40c5c485bb681c7083666df7d677fb7898b7151
miyu <a82902zz3@gmail.com>
2023-08-16 17:09:34 +0800

    456

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { ArticlesService } from './../articles.service';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-tags',
   templateUrl: './tags.component.html',
-  styleUrls: ['./tags.component.css']
+  styleUrls: ['./tags.component.css'],
 })
-export class TagsComponent {
+export class TagsComponent implements OnInit {
+  tags: string[] = [];
 
+  @Output() choose = new EventEmitter<string>();
+
+  constructor(private ArticlesService: ArticlesService) {}
+
+  ngOnInit(): void {
+    this.ArticlesService.getTags().subscribe((res) => {
+      this.tags = res;
+    });
+  }
+
+  chooseTag(tag: string) {
+    this.choose.emit(tag);
+  }
 }
